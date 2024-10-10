7310aba084493937d9b85359958f8713d97853f2
Zeref <krishh16@yahoo.com>
2024-10-10 23:25:58 +0530

    added the nft minter program

--- Solana-Programs/token/mock/programs/mock/src/lib.rs
+++ Solana-Programs/token/mock/programs/mock/src/lib.rs
@@ -1,26 +1,35 @@
 use anchor_lang::prelude::*;
 use anchor_spl::{
-    token::{Mint, Token},
+    token::{Token, Mint, mint_to, TokenAccount, MintTo},
+    associated_token::AssociatedToken,
     metadata::{
-        create_metadata_accounts_v3,mpl_token_metadata::types::DataV2,
-        CreateMetadataAccountsV3, Metadata
+        create_master_edition_v3, create_metadata_accounts_v3,
+        CreateMasterEditionV3, CreateMetadataAccountsV3, mpl_token_metadata::types::DataV2,
+        Metadata
     }
 };
 
-declare_id!("8vaEmpFzZKoh6zeMFSn7jdzBcbsJWqCgy9vkjTajQwvk");
+declare_id!("A2YnRqimpYk7KkW67ffMoNQtE9X3wLZJC8BcdYxbj3Sh");
 
 #[program]
 pub mod mock {
     use super::*;
 
-    pub fn create_token(ctx: Context<CreateTokenMint>, 
-        _token_decimal: u8, 
-        token_name: String, 
-        token_symbol: String,
-        token_uri: String
-    ) -> Result<()> {
+    pub fn create_nft(ctx: Context<CreateNFTToken>, nft_name: String, nft_symbol: String, nft_uri: String) -> Result<()> {
+        msg!("Minting token");
 
-        msg!("Initializing the metadata_account");
+        mint_to(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                MintTo { mint:ctx.accounts.mint_account.to_account_info(), 
+                    to: ctx.accounts.associated_token_account.to_account_info(),
+                     authority: ctx.accounts.payer.to_account_info() 
+                    }
+            ),
+            1
+        )?;
+
+        msg!("creating metadata account");
 
         create_metadata_accounts_v3(
             CpiContext::new(
@@ -28,51 +37,80 @@ pub mod mock {
                 CreateMetadataAccountsV3{
                     metadata: ctx.accounts.metadata_account.to_account_info(),
                     mint: ctx.accounts.mint_account.to_account_info(),
-                    payer: ctx.accounts.payer.to_account_info(),
-                    rent: ctx.accounts.rent.to_account_info(),
                     mint_authority: ctx.accounts.payer.to_account_info(),
+                    payer: ctx.accounts.payer.to_account_info(),
+                    update_authority: ctx.accounts.payer.to_account_info(),
                     system_program: ctx.accounts.system_program.to_account_info(),
-                    update_authority: ctx.accounts.payer.to_account_info()
+                    rent: ctx.accounts.rent.to_account_info()
                 }
-            ),
-            DataV2 { name: token_name, symbol: token_symbol, uri: token_uri, seller_fee_basis_points: 0, creators: None, collection: None, uses: None },
-            false, // This token isn't mutable
-            true, // update authority is a signer
-            None // Collection details
-        )?;
+            ), 
+            DataV2 { name: nft_name, symbol: nft_symbol, uri: nft_uri, seller_fee_basis_points: 0, creators: None, collection: None, uses: None },
+            false,
+            true, 
+            None)?;
 
-        msg!("Token created");
+        msg!("Creating Master Edition account");
+
+        create_master_edition_v3(
+            CpiContext::new(
+                ctx.accounts.token_metadata_program.to_account_info(),
+                CreateMasterEditionV3{
+                    edition: ctx.accounts.edition_account.to_account_info(),
+                    mint: ctx.accounts.mint_account.to_account_info(),
+                    update_authority: ctx.accounts.payer.to_account_info(),
+                    mint_authority: ctx.accounts.payer.to_account_info(),
+                    payer: ctx.accounts.payer.to_account_info(),
+                    metadata: ctx.accounts.metadata_account.to_account_info(),
+                    token_program: ctx.accounts.token_program.to_account_info(),
+                    system_program: ctx.accounts.system_program.to_account_info(),
+                    rent: ctx.accounts.rent.to_account_info()
+                }
+            )
+        , None)?;// The second argument being none suggest that there isn't going to be any copies/editions of the specified NFT
+
+        msg!("NFT minted");
 
         Ok(())
     }
-
-
 }
 
 #[derive(Accounts)]
-#[instruction(_token_decimals: u8)]
-pub struct CreateTokenMint<'info> {
+pub struct CreateNFTToken<'info> {
     #[account(mut)]
     pub payer: Signer<'info>,
-
-    /// CHECK: Validate address by deriving pda
+    /// Check: This ain't safe
     #[account(
         mut,
         seeds = [b"metadata", token_metadata_program.key().as_ref(), mint_account.key().as_ref()],
         bump,
-        seeds::program = token_metadata_program.key(),
+        seeds::program = token_metadata_program.key()
+    )]
+    pub edition_account: UncheckedAccount<'info>,
+    #[account(
+        mut,
+        seeds = [b"metadata", token_metadata_program.key().as_ref(), mint_account.key().as_ref(), b"edition"],
+        bump,
+        seeds::program = token_metadata_program.key()
     )]
     pub metadata_account: UncheckedAccount<'info>,
-    // Create new mint account
     #[account(
-        init,
+        init, 
+        mint::decimals = 0,
         payer = payer,
-        mint::decimals = _token_decimals,
         mint::authority = payer.key(),
+        mint::freeze_authority = payer.key()
     )]
     pub mint_account: Account<'info, Mint>,
-    pub token_metadata_program: Program<'info, Metadata>,
-    pub token_program: Program<'info, Token>,
+    #[account(
+        init, 
+        payer = payer,
+        associated_token::mint = mint_account,
+        associated_token::authority = payer
+    )]
+    pub associated_token_account: Account<'info, TokenAccount>,
     pub system_program: Program<'info, System>,
-    pub rent: Sysvar<'info, Rent>,
+    pub token_program: Program<'info, Token>,
+    pub token_metadata_program: Program<'info, Metadata>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub rent: Sysvar<'info, Rent>
 }
--- Solana-Programs/token/mock/tests/mock.ts
+++ Solana-Programs/token/mock/tests/mock.ts
@@ -1,36 +1,16 @@
 import * as anchor from "@coral-xyz/anchor";
-import {Keypair} from "@solana/web3.js";
-
 import { Program } from "@coral-xyz/anchor";
 import { Mock } from "../target/types/mock";
 
-const TOKEN_METADATA_PROGRAM_ID = new anchor.web3.PublicKey("metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s");
-
-describe("mock", async () => {
+describe("mock", () => {
   // Configure the client to use the local cluster.
-  const provider = anchor.AnchorProvider.env();
-  anchor.setProvider(provider);
+  anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.Mock as Program<Mock>;
-  const payer =  provider.wallet as anchor.Wallet;
 
-  const metadata = {
-    name: "Zeref",
-    symbol: "ZRF",
-    uri: "https://raw.githubusercontent.com/solana-developers/program-examples/new-examples/tokens/tokens/.assets/spl-token.json"
-  }
-
-  it("Initializes the token", async () => {
-    const mintAccount = new Keypair();
-
-    const txn = await program.methods.createToken(9, metadata.name, metadata.symbol, metadata.uri)
-    .accounts({
-      payer: payer.publicKey,
-      mintAccount: mintAccount.publicKey,      
-    })
-    .signers([mintAccount])
-    .rpc();
-
-    console.log(`Created a Token here: ${mintAccount.publicKey.toBase58()}`)
-  })
+  it("Is initialized!", async () => {
+    // Add your test here.
+    const tx = await program.methods.initialize().rpc();
+    console.log("Your transaction signature", tx);
+  });
 });
