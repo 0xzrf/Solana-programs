63405b5bc82b7cfc0e8d5a97ec2df0a15f49db7d
Zeref <krishh16@yahoo.com>
2024-10-22 17:57:28 +0530

    added the close account instruction

--- basics/mock_native/src/instruction/close.rs
+++ basics/mock_native/src/instruction/close.rs
@@ -1,5 +1,6 @@
+use borsh::BorshSerialize;
 use solana_program::{
-    account_info::AccountInfo, entrypoint::ProgramResult, program::invoke_signed, system_instruction
+    account_info::{next_account_info, AccountInfo}, entrypoint::ProgramResult, program::invoke_signed, rent::Rent, system_instruction, sysvar::Sysvar
 };
 
 
@@ -7,5 +8,24 @@ use solana_program::{
 pub fn close(
     account_infos: &[AccountInfo]
 ) -> ProgramResult {
+
+    let account_iter = &mut account_infos.iter();
+
+    let payer = next_account_info(account_iter)?;
+    let target_account = next_account_info(account_iter)?;
+    let system_program = next_account_info(account_iter)?;
+
+    let account_span = 0usize; // calculating the actual account size instead of the target account to see what is the rent expempt money we need for this   
+
+    let diff = target_account.lamports();
+
+    **target_account.lamports.borrow_mut() -= diff;
+    **payer.lamports.borrow_mut() += diff;
+
+    // Realloc the account to zero usize, meaning just remove the account and it's fund from existance
+    target_account.realloc(account_span,true)?;
+
+    target_account.assign(system_program.key);
+
     Ok(())
 }
