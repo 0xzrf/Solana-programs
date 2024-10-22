c2414d1d443233d04d6b373e368b9d11814faa4b
Zeref <krishh16@yahoo.com>
2024-10-22 17:39:14 +0530

    created the create_account instruction

--- basics/mock_native/src/instruction/create.rs
+++ basics/mock_native/src/instruction/create.rs
@@ -1,13 +1,46 @@
 use solana_program::{
-    account_info::AccountInfo, entrypoint::ProgramResult, program::invoke_signed, pubkey::Pubkey, system_instruction
+    account_info::{next_account_info, AccountInfo},
+    entrypoint::ProgramResult,
+    program::invoke_signed,
+    pubkey::Pubkey,
+    rent::Rent,
+    system_instruction,
+    sysvar::Sysvar,
 };
 
 use crate::states::User;
+use borsh::BorshSerialize;
 
-pub fn create(
-    program_id: &Pubkey,
-    account_infos: &[AccountInfo],
-    data: User
-) -> ProgramResult {
+pub fn create(program_id: &Pubkey, account_infos: &[AccountInfo], data: User) -> ProgramResult {
+    let account_iter = &mut account_infos.iter();
+    let payer = next_account_info(account_iter)?;
+    let target_account = next_account_info(account_iter)?;
+    let system_program = next_account_info(account_iter)?;
+
+    let account_span = (data.try_to_vec()?).len();
+    let lamports_required = (Rent::get()?).minimum_balance(account_span);
+
+    let seeds = &[User::seed_prefix, payer.key.as_ref()];
+
+    let (_, bump) = Pubkey::find_program_address(seeds, program_id);
+
+    invoke_signed(
+        &system_instruction::create_account(
+            payer.key,
+            target_account.key,
+            lamports_required,
+            account_span as u64,
+            payer.key,
+        ),
+        &[
+            payer.clone(),
+            target_account.clone(),
+            system_program.clone(),
+        ],
+        &[&[User::seed_prefix, payer.key.as_ref(), &[bump]]],
+    )?;
+
+    data.serialize(&mut &mut target_account.data.borrow_mut()[..])?;
+
     Ok(())
 }
