f6d71a813df51645e0204e068ae236aa9263d7e1
Zeref <krishh16@yahoo.com>
2024-10-24 02:18:26 +0530

    added the pda storing the data functionalities

--- basics/mock_native/src/processor.rs
+++ basics/mock_native/src/processor.rs
@@ -6,6 +6,7 @@ use solana_program::{
 };
 
 use crate::states::*;
+use crate::instruction::{create::create_pda, increment::increment};
 
 pub fn process_instruction(
     program_id: &Pubkey,
@@ -13,12 +14,12 @@ pub fn process_instruction(
     instruction_data: &[u8]
 ) -> ProgramResult {
 
-    if let Ok(page_visits) = IncrementPageVists::try_from_slice(instruction_data) {
-
+    if let Ok(page_visits) = PageVisits::try_from_slice(instruction_data) {
+        create_pda(program_id, account_infos, page_visits)?;
     } 
 
     if IncrementPageVists::try_from_slice(instruction_data).is_ok() {
-
+        increment(account_infos)?;
     }
 
     Ok(())
