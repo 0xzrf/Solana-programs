98d0dcdaffac257f0ee83c73933c44f48f654298
Zeref <krishh16@yahoo.com>
2024-10-25 23:46:54 +0530

    added tests for the counter

--- basics/counter/native/tests/test.ts
+++ basics/counter/native/tests/test.ts
@@ -1,54 +1,77 @@
-import {ProgramTestContext, start} from "solana-bankrun"
-import { describe, test } from "node:test"
-import { assert } from "chai"
-import {PublicKey, Transaction, TransactionInstruction, Keypair} from "@solana/web3.js"
-import * as borsh from "borsh"
+import { describe, test } from 'mocha';
+import { assert, expect } from 'chai';
+import { Keypair, PublicKey, SystemProgram, Transaction, TransactionInstruction } from '@solana/web3.js';
+import { BanksClient, Rent, start , } from 'solana-bankrun';
+import { BN } from 'bn.js';
+// import { COUNTER_ACCOUNT_SIZE, PROGRAM_ID, createIncrementInstruction, deserializeCounterAccount } from '../ts';
 
-class AddressInitializer {
-    constructor(properties) {
-        for (const [key, val] of Object.entries(properties)) this[key]= val;
+describe('Counter Solana Native',  () => {
+  console.log("describe started")
+
+  let programId: PublicKey,  client:BanksClient, payer: Keypair, blockhash: string, rent: Rent; 
+
+  beforeEach( async () => {
+     programId = PublicKey.unique();
+    const context = await start([{ name: 'counter_solana_native', programId }], []);
+    // Randomly generate the program keypair and load the program to solana-bankrun
+    client = context.banksClient;
+    // Get the payer keypair from the context, this will be used to sign transactions with enough lamports
+    payer = context.payer;
+    blockhash = context.lastBlockhash;
+    rent = await client.getRent();
+  })
+
+  // Get the rent object to calculate rent for the accounts
+  test('check the logs', async () => {
+    const counter = Keypair.generate();
+    const createAccountInstruction: TransactionInstruction = SystemProgram.createAccount({
+      fromPubkey: payer.publicKey,
+      lamports: Number(rent.minimumBalance(BigInt(8))),
+      newAccountPubkey: counter.publicKey,
+      programId,
+      space: 8
+    });
+    
+
+    const incrementInstruction: TransactionInstruction = new TransactionInstruction({
+      programId: programId,
+      keys: [
+        {
+          isSigner: true,
+          isWritable: false,
+          pubkey: counter.publicKey
+        }
+      ],
+      data: new Buffer([0x0])
+    }) 
+
+
+    const tx = new Transaction().add(createAccountInstruction).add(incrementInstruction);
+
+
+    tx.feePayer = payer.publicKey;
+
+    tx.recentBlockhash = blockhash;
+    tx.sign(payer, counter);
+    await client.processTransaction(tx);
+
+    const counterAccouuntInfo = await client.getAccount(counter.publicKey);
+
+    assert(counterAccouuntInfo, "Account was not created")
+
+    console.log(counterAccouuntInfo)
+    console.log(counterAccouuntInfo.owner.toString(), programId.toString())
+
+    const data = Buffer.from(counterAccouuntInfo.data)
+
+    if (data.byteLength != 8) {
+      new Error("The size ain't good enough")
     }
-}
+    const counterAccount = {count: new BN(data, 'le')} // The data variable of the account will contain the state variables
 
-class AddressInfo extends AddressInitializer {
-    street: any;
-    city: any;
-    name: any;
-    house_amount: any;
+    assert(counterAccount.count.toNumber() == 1, "Count ain't even 1")
 
-    toBuffer() {
-        return Buffer.from(borsh.serialize(AddressInfoSchema, this))
-    }
+    console.log(counterAccount.count)
 
-    static fromBufffer(buffer: Buffer) {
-        return borsh.deserialize(AddressInfoSchema, AddressInfo, buffer)
-    }
-}
-
-const AddressInfoSchema = new Map([
-    [
-      AddressInfo, 
-      {
-        kind: 'struct',
-        fields: [
-          ['name', 'string'],
-          ['house_number', 'u8'],
-          ['street', 'string'],
-          ['city', 'string'],
-        ],
-      },
-    ],
-  ]);
-
-describe("Tests the functionality of the solana native program", async () => {
-    const addresssInfoAccount = Keypair.generate();
-    const PROGRAM_ID = PublicKey.unique();
-    const context = await start([{name: "mock_native", programId: PROGRAM_ID}], [])
-    const client = context.banksClient
-    const payer = context.payer;
-
-    it("Tests whether account is being created or not", async () => {
-        const balance = await client.getBalance(payer.publicKey)
-        console.log(balance)
-    })
+  });
 })
